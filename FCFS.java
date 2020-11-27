1ab3f68f4c0989932aa6a35afe1f48a98dd6c8bb
Hemanth <hemanth.kotagiri43@gmail.com>
2020-11-27 13:01:31 +0530

    Complete implementation in Java

--- FCFS.java
+++ FCFS.java
@@ -28,23 +28,60 @@ class FCFS {
         }
     }
 
+    public double compute_average(int[] arr) {
+        double avg = 0;
+        for(int element : arr) {
+            avg += element;
+        }
+        return avg/arr.length;
+    }
+
     public void first_come_first_serve() {
 
         int temp[][] = this.times;
 
         Arrays.sort(temp, Comparator.comparingDouble(element -> element[0]));
 
+        int[] completion_times = new int[n];
+        int[] turn_around_times = new int[n];
+        int[] waiting_times = new int[n];
+
+
+        for(int i = 0; i < n; i++) {
+            int[] current_process = temp[i];
+
+            if(i == 0) {
+                completion_times[i] = Math.addExact(current_process[0], current_process[1]);
+            }
+            else {
+                int current_completion_time = completion_times[i - 1] + current_process[1];
+
+                // Handling the case when the cpu is idle
+                int idle_period = current_process[0] - completion_times[i - 1];
+                if (idle_period > 0) {
+                    current_completion_time += idle_period;
+                }
+
+                completion_times[i] = current_completion_time;
+            }
+
+            turn_around_times[i] = completion_times[i] - current_process[0];
+            waiting_times[i] = turn_around_times[i] - current_process[1];
+        }
+
+        System.out.println("CT | TAT | WT");
         for(int i=0; i<this.n; i++) {
-            System.out.print(temp[i][0] + " " + temp[i][1]);
+            System.out.print(completion_times[i] + " | " + turn_around_times[i] + " | " + waiting_times[i]);
             System.out.println();
         }
+        System.out.println("Average Turn Around Time: " + this.compute_average(turn_around_times));
+        System.out.println("Average Waiting Time: " + this.compute_average(waiting_times));
     }
 
 
 
     public static void main(String[] args) {
         FCFS demo = new FCFS();
-        // demo.show_processess();
         demo.first_come_first_serve();
     }
 }
